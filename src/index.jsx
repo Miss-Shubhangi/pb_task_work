051bbfcb68d6b8a39c6f14081f65e38daa4bb216
Miss-Shubhangi <shubhangi.s.pawar2007@gmail.com>
2024-10-11 14:20:53 +0530

    Initial setup

---
+++ src/index.jsx
@@ -0,0 +1,22 @@
+import ReactDOM from 'react-dom/client';
+import {createBrowserRouter,RouterProvider} from "react-router-dom";
+import './index.css';
+//import Sakshi from './views/sakshi';
+import PlanPricing from './Views/PlanPricing/PlanPricing.jsx';
+//import Registration from './views/Registration/Registration';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<PlanPricing/>
+  },
+  {
+    path : "/planpricing",
+    element: <PlanPricing/>
+  },
+ 
+])
+root.render(
+  <RouterProvider router={router}/>
+)
